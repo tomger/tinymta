3952abf3e53202a8fe32b8ee112acb173291254f
tomg <tom.germeau@gmail.com>
2018-08-12 17:05:31 -0400

    bergen 2/3

--- server.js
+++ server.js
@@ -21,10 +21,11 @@ csvParse(file, {
 
 function getTrainLines(success) {
   var rv = [];
+  // 16
 
   var requestSettings = {
     method: 'GET',
-    url: `http://datamine.mta.info/mta_esi.php?key=${process.env.MTA_API_KEY}&feed_id=16`,
+    url: `http://datamine.mta.info/mta_esi.php?key=${process.env.MTA_API_KEY}&feed_id=1`,
     encoding: null
   };
   request(requestSettings, function (error, response, body) {
@@ -35,25 +36,28 @@ function getTrainLines(success) {
         if (!entity.trip_update) {
           return false;
         }
-        if (entity.trip_update.trip.route_id !== 'Q') {
-          return false;
-        }
+        // if (entity.trip_update.trip.route_id !== 'Q') {
+        //   return false;
+        // }
         return true;
       })
       rv.push(trains.length);
       trains.forEach(function(entity) {
-        rv.push(`🚂  ${entity.trip_update.trip.route_id} ${entity.id}`);
         entity.trip_update.stop_time_update //.splice(0, 3)
+        .filter(update => {
+          return stops[update.stop_id] && update.arrival;
+        })
+        .filter(update => {
+          return update.stop_id === '236N'
+        })
         .forEach(function(update) {
           // console.log(entity.trip_update.stop_time_update);
-          if (update.stop_id === 'Q05S' && stops[update.stop_id]) {
-            rv.push([
-              update.stop_id,
-              stops[update.stop_id].stop_name,
-              `<script>document.write((new Date(${update.arrival.time.low*1000})).toLocaleTimeString())</script>`
-            ].join(' '));
-
-          };
+          rv.push({
+            route: entity.trip_update.trip.route_id,
+            stop_id: update.stop_id,
+            stop: stops[update.stop_id].stop_name,
+            time: update.arrival.time.low*1000
+          });
         })
       });
       success(rv);
@@ -66,10 +70,14 @@ app.set('port', (process.env.PORT || 5000))
 // app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-
-  getTrainLines((rv)=>
-    response.send(rv.join('<br/>'))
-  );
+  getTrainLines((rv)=> {
+    rv.sort((a, b) => {
+      return a.time - b.time;
+    })
+    response.send(rv.map(train => {
+      return `🚂 ${train.route} <script>document.write((new Date(${train.time})).toLocaleTimeString())</script>`
+    }).join('<br/>'))
+  });
 })
 
 app.listen(app.get('port'), function() {
